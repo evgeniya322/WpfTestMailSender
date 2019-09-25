0eeb2318d56e8e99288d2f44c5ab32ee834e06b4
Evgeniya <zbizk322@mail.ru>
2019-09-25 17:13:42 +0700

    Кнопки перехода между TabItem

---
+++ WpfTestMailSender/Controls/TabItemSwicher.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTestMailSender.Controls
+{
+    /// <summary>
+    /// Логика взаимодействия для TabItemSwicher.xaml
+    /// </summary>
+    public partial class TabItemSwicher : UserControl
+    {
+        public TabItemSwicher()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler LeftButtonClick;
+        public event EventHandler RightButtonClick;
+
+        public bool LeftButtonVisible
+        {
+            get => LeftButton.Visibility == Visibility;
+            set => LeftButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public bool RightButtonVisible
+        {
+            get => RightButton.Visibility == Visibility;
+            set => RightButton.Visibility = value ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!(e.Source is Button button)) return;
+
+            switch (button.Name)
+            {
+                case "LeftButton":
+                    LeftButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+                case "RightButton":
+                    RightButtonClick?.Invoke(this, EventArgs.Empty);
+                    break;
+            }
+        }
+    }
+}
--- WpfTestMailSender/WpfMailSender.xaml.cs
+++ WpfTestMailSender/WpfMailSender.xaml.cs
@@ -14,6 +14,7 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Net;
 using System.Net.Mail;
+using WpfTestMailSender.Controls;
 
 namespace WpfTestMailSender
 {
@@ -38,7 +39,35 @@ namespace WpfTestMailSender
             Close();
         }
 
-       
+        private void TabItemSwicher_LeftButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is TabItemSwicher switcher)) return;
 
+            if (MainTabCantrol.SelectedIndex == 0) return;
+
+            if (switcher.RightButtonVisible == false) switcher.RightButtonVisible = true;
+            MainTabCantrol.SelectedIndex--;
+            if (MainTabCantrol.SelectedIndex == 0)
+            {
+                switcher.LeftButtonVisible = false;
+            }
+        }
+
+        private void TabItemSwicher_RightButtonClick(object sender, EventArgs e)
+        {
+            if (!(sender is TabItemSwicher switcher)) return;
+
+            var tab_count = MainTabCantrol.Items.Count;
+
+            if (MainTabCantrol.SelectedIndex == tab_count - 1) return;
+
+            if (switcher.LeftButtonVisible == false) switcher.LeftButtonVisible = true;
+            MainTabCantrol.SelectedIndex++;
+
+            if (MainTabCantrol.SelectedIndex == MainTabCantrol.Items.Count - 1)
+            {
+                switcher.RightButtonVisible = false;
+            }
+        }
     }
 }
